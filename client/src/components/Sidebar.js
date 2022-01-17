a95569c592afad62e189d5a99dba597edec89521
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-01-17 05:59:37 +0530

    Sidebar UI Changes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,6 +4,55 @@ import { IoEllipsisVertical } from "react-icons/io5"
 import { RiMessage3Fill, RiCheckboxBlankCircleLine } from "react-icons/ri"
 
 const Sidebar = () => {
+
+    let contacts = [
+        {
+            id: 1,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 2
+        },
+        {
+            id: 2,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 2
+        },
+        {
+            id: 3,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 2
+        },
+        {
+            id: 4,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 1000
+        },
+        {
+            id: 5,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 100
+        },
+        {
+            id: 6,
+            conversationName: "Chaitanya",
+            lastMessageTime: "9:30 pm",
+            lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sequi nostrum quasi laudantium modi dolore! Reprehenderit neque rem, esse sequi impedit doloribus quasi blanditiis facere sit exercitationem veniam, quos labore, minus necessitatibus incidunt et expedita dignissimos nisi tempora tempore iusto quaerat. Consectetur fuga eum laudantium soluta. Ducimus nesciunt repellendus dicta.",
+            unseenMessages: 10
+        }
+    ];
+
+    contacts = [];
+
+
     return (
         <div className='sidebar-div'>
             <div className="sidebar-header">
@@ -17,23 +66,34 @@ const Sidebar = () => {
                 </div>
             </div>
             <div className="sidebar-body">
-                <div>
-                    <div className="user-wrapper">
-                        <div className="user">
-                            <FaUser className="user-img" />
+                {
+                    contacts.length > 0 ?
+                        contacts.map(contact => (
+                            <div key={contact.id}>
+                                <div className="user-wrapper">
+                                    <div className="user">
+                                        <FaUser className="user-img" />
+                                    </div>
+                                </div>
+                                <div className="chat-wrapper">
+                                    <div className="conversation-name-wrapper">
+                                        <h3 className="conversation-name">{contact.conversationName}</h3>
+                                        <span className="conversation-time">{contact.lastMessageTime}</span>
+                                    </div>
+                                    <div className="recent-chat-wrapper">
+                                        <p className="recent-chat">{contact.lastMessage}</p>
+                                        <div className="messages-number">
+                                            <span>{contact.unseenMessages}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )) :
+                        <div className="no-conversations">
+                            <p>You have no conversations</p>
+                            <div className="btn">Create One</div>
                         </div>
-                    </div>
-                    <div className="chat-wrapper">
-                        <div className="conversation-name-wrapper">
-                            <h3 className="conversation-name">Chaitanya</h3>
-                            <span className="conversation-time">9:30 pm</span>
-                        </div>
-                        <div className="recent-chat-wrapper">
-                            <p className="recent-chat">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, accusamus quam nisi, nemo earum iure perspiciatis voluptas porro repellendus quae consectetur numquam necessitatibus dicta est explicabo error quod illo quaerat eaque molestias eligendi quos reiciendis placeat asperiores. Tenetur eveniet hic, facilis itaque esse veniam ea temporibus dolorum, ad laudantium dicta!</p>
-                            <div className="messages-number">1</div>
-                        </div>
-                    </div>
-                </div>
+                }
             </div>
         </div>
     )
